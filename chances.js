f9509e10d1260e833549ba5dd7f7a7ed429b2477
David <davidheis@hotmail.com>
2017-01-05 19:26:49 +0100

    Create chances.js

---
+++ chances.js
@@ -0,0 +1,19 @@
+function guess(x){
+	var limit = 10001
+	var count = 0
+	var random = Math.floor(Math.random() * limit)
+	if (x === random) {
+		return '1st go!!!'
+	}
+	if (x > limit - 1 || x < 0) {
+		return 'number must be lower than 100, and more than 0'
+	} else {
+		while(x !== random){
+		console.log(random)
+		random = Math.floor(Math.random() * limit)
+		count++
+	}
+		return 'You got it! ' + x + ' was your number. It took ' + count + ' tries.'
+
+	}
+}
